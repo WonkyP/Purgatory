552085df2c1d485e01c5fb57b7f67c2d3f43dc89
rebo95 <arebored@ucm.es>
2019-03-07 15:06:59 +0100

    started new menu controller interaction

---
+++ Unity/Assets/TheGame/_Script/MenusManager/MenuBehaviourForController/CustomList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class CustomList
+{
+    public List<Image> fils;
+}
---
+++ Unity/Assets/TheGame/_Script/MenusManager/MenuBehaviourForController/InventoryControllerInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class InventoryControllerInteraction : MonoBehaviour
+{
+    //[SerializeField]
+    //public List<Image> fils;
+    [System.Serializable]
+    public class Custom
+    {
+        public List<Image> images;
+    }
+
+    public List<Custom> inventario;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
