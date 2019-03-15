2d82fff5e0326b991ca77bd63a22c3ce6f719446
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-03-15 13:29:50 +0100

    start of the attack collider on and off

---
+++ Unity/Assets/TheGame/_Script/Weapons/WeaponAttackState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAttackState : MonoBehaviour
+{
+    [SerializeField]
+    Collider WeaponCollider;
+
+    public void turnColliderOn()
+    {
+        WeaponCollider.enabled = true;
+    }
+    public void turnColliderOff()
+    {
+        WeaponCollider.enabled = true;
+    }
+}
