77f8f11afea7023d4778656bfb3c0a9c38593805
Carl Christian Von Munthe af Morgenstierne <carlmorgenstierne@yahoo.com>
2019-03-15 16:00:45 +0100

    Update WeaponCollider.cs

--- Unity/Assets/TheGame/_Script/Weapons/WeaponCollider.cs
+++ Unity/Assets/TheGame/_Script/Weapons/WeaponCollider.cs
@@ -6,11 +6,12 @@ public class WeaponCollider : MonoBehaviour
 {
     public Player PlayerScript;
     int playerId;
-
+    Collider col;
     // Start is called before the first frame update
     void Start()
     {
         playerId = PlayerScript.Player_Id;
+        col = GetComponent<Collider>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +20,7 @@ public class WeaponCollider : MonoBehaviour
             Player pS = other.GetComponent<Player>();
             if (pS.Player_Id != playerId)
             {
+                col.enabled = false;
                 pS.GettingHit(20f);
             }
         }
